6506418fd73e693c46c2524c4fa1361c8588f951
panagiotis <peteraki137@gmail.com>
2019-08-29 16:26:57 +0300

    fix back button

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var LocalStrategy = require("passport-local");
 var methodOverride = require("method-override");
 var passportLocalMongoose = require("passport-local-mongoose");
 var bodyParser = require("body-parser");
-var flash = require("connect-flash");
+var flash = require("connect-flash"); 
 var https = require('https');
 var fs = require('fs');
 var dateFormat = require('dateformat');
@@ -914,65 +914,7 @@ app.get("/chats/:currentUserId/chat/:otherUserId/delete", function(req, res) {
 			console.log(error);
 		}
 		else {
-<<<<<<< HEAD
 			if(foundChat !== null) {
-				Chat.findOneAndDelete({_id : foundChat._id}, {useFindAndModify: false }, function(error, deletedChat) {
-					if(error) {
-						console.log(error);
-					}
-					else {
-						User.findOne({								 // delete chat from first user
-							_id: req.params.currentUserId
-						}, function(error, foundUser) {
-								if(error) {
-									console.log(error)
-								}
-								else {
-									 for (var i = 0; i < foundUser.chats.length; i++) {
-									 		console.log("H00000000000000");
-									 		if(foundUser.chats[i].chat !== null && deletedChat !== null) {
-									 			console.log("HIIIIIIIIIIIIIIIIIIIi");
-										 		if(String(foundUser.chats[i].chat._id) === String(deletedChat._id) ) {
-										 			console.log("H2222222222222222");
-										 			foundUser.chats.splice(i,1);
-										 			foundUser.save();
-													User.findOne({                     // delete chat from second user
-														_id: req.params.otherUserId
-													}, function(error, foundUser2) {
-															if(error) {
-																console.log(error)
-															}
-															else {
-																 for (var i = 0; i < foundUser2.chats.length; i++) {
-																 		console.log("H00000000000000");
-																 		if(foundUser2.chats[i].chat !== null && deletedChat !== null) {
-																 			console.log("HIIIIIIIIIIIIIIIIIIIi");
-																	 		if(String(foundUser2.chats[i].chat._id) === String(deletedChat._id) ) {
-																	 			console.log("H2222222222222222");
-																	 			foundUser2.chats.splice(i,1);
-																	 			foundUser2.save();
-																	 			
-																	 			res.redirect("/chats");
-																	 		}
-																 		}
-																}								
-															}
-													})
-										 		}
-									 		}
-									}								
-								}
-						})
-						
-					}
-				});
-			}
-			else {
-				res.redirect("/chats");
-			}
-		}
-				});	
-=======
 			Chat.findOneAndDelete({_id : foundChat._id}, {useFindAndModify: false }, function(error, deletedChat) {
 				if(error) {
 					console.log(error);
@@ -1008,25 +950,27 @@ app.get("/chats/:currentUserId/chat/:otherUserId/delete", function(req, res) {
 																 			console.log("H2222222222222222");
 																 			foundUser2.chats.splice(i,1);
 																 			foundUser2.save();
-
+																 			
 																 			res.redirect("/chats");
 																 		}
 															 		}
-															}
+															}								
 														}
 												})
 									 		}
 								 		}
-								}
+								}								
 							}
 					})
-
-
+				
+					
 				}
 			});
+		}	
+		else 
+			res.redirect("/chats");
 		}
-	});
->>>>>>> 298a762bfb5d0e47076f26a26bf9a60501b0c631
+	});	
 });
 
 
